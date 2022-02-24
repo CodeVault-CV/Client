86c04b3ace6f4dc30f2910ad3b10b966f36dacce
woong-jae <jwchung0828@gmail.com>
2022-02-24 18:22:46 +0900

    추가: CSS Reset

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,18 +1,26 @@
 const Main = `
     <div class="main-container">
-        <h1><알공></h1>
-        <p>
-            알고리즘 스터디를 가장 쉽게 시작할 수 있는 곳
-        </p>
-        <github-login></github-login>
+        <div class="main-component">
+            <h1><알공></h1>
+            <p>
+                알고리즘 스터디를 가장 쉽게 시작할 수 있는 곳
+            </p>
+            <github-login></github-login>
+        </div>
     </div>
     <style>
         .main-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 85vh;
+        }
+        .main-component {
             width: 500px;
             margin: auto;
             padding: 30px;
         }
-        .main-container h1 {
+        .main-component h1 {
             text-align: center;
         }
     </style>
