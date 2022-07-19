996016d2446a491d718f292cf9a8d24469837b83
woong-jae <jwchung0828@gmail.com>
2022-07-19 16:42:28 +0900

    #31 test: 스터디 id로 스터디 조회 api 모킹

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,6 +3,7 @@ import { rest } from "msw";
 const baseURL = process.env.REACT_APP_SERVER_BASE_URL;
 
 export const handlers = [
+  // GET
   rest.get(baseURL + "/study/list", (req, res, ctx) => {
     return res(
       ctx.json({
@@ -11,15 +12,48 @@ export const handlers = [
         data: [
           {
             id: "test1",
-            name: "알고리즘 박살"
+            name: "알고리즘 박살",
           },
           {
             id: "test2",
-            name: "카카오 문제 박살내기"
-          }
-        ]
+            name: "카카오 문제 박살내기",
+          },
+        ],
       })
-    )
+    );
+  }),
+  rest.get(baseURL + "/study/:studyId", (req, res, ctx) => {
+    const id = req.params.studyId;
+
+    return res(
+      ctx.json({
+        status: 200,
+        message: "SUCCESS",
+        data: {
+          id,
+          name: (id === "test1" ? "알고리즘 박살" : "카카오 문제 박살내기"),
+          url: "www.github.com",
+          members: [
+            {
+              name: "woong-jae",
+              image: "",
+            },
+            {
+              name: "Kingdonggyu",
+              image: "",
+            },
+            {
+              name: "SeongukBaek",
+              image: "",
+            },
+            {
+              name: "Go-Jaecheol",
+              image: "",
+            },
+          ],
+        },
+      })
+    );
   }),
   rest.get(baseURL + "/session/:sessionId", (req, res, ctx) => {
     return res(
