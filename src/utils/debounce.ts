2195f8c8e38209d90f85fc3d5c8ed22cc757d17e
KingDonggyu <dgkim69166916@gmail.com>
2022-07-24 17:33:12 +0900

    feat #43 debounce util 함수 구현 및 적용

---
+++ src/utils/debounce.ts
@@ -0,0 +1,14 @@
+export default function debounce(func: any, delay: number = 1000) {
+  let timeout: NodeJS.Timeout | null;
+
+  return function (...args: any) {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    
+    timeout = setTimeout(() => {
+      timeout = null;
+      func(...args);
+    }, delay);
+  };
+}
