01ff5728c9bd051821e8dac27e447610d847e095
woong-jae <jwchung0828@gmail.com>
2022-08-17 17:22:13 +0900

    fix: session header에 wrapper 추가

--- src/components/pages/Session/SessionHeader/SessionHeader.tsx
+++ src/components/pages/Session/SessionHeader/SessionHeader.tsx
@@ -7,25 +7,32 @@ import Header from "../../../blocks/Header";
 import SessionUpdate from "./SessionUpdate";
 
 import Session from "../../../../core/types/Session";
+import Wrapper from "../../../blocks/Wrapper";
 
 type SessionHeaderProps = {
-  session: Session,
+  session: Session;
   handleDelete(event: MouseEvent<HTMLButtonElement>): void;
 };
 
 export default function SessionHeader({ session, handleDelete }: SessionHeaderProps) {
-  
   return (
-    <Header title={session.name}>
-      <DateLabel start={new Date(session.start)} end={session.end} />
-      <Box sx={{ display: "flex" }}>
-        <SessionUpdate id={session.id} name={session.name} start={session.start} end={session.end} />
-        <Tooltip title="세션 삭제하기" arrow>
-          <IconButton onClick={handleDelete}>
-            <DeleteForeverIcon />
-          </IconButton>
-        </Tooltip>
-      </Box>
-    </Header>
+    <Wrapper>
+      <Header title={session.name}>
+        <DateLabel start={new Date(session.start)} end={session.end} />
+        <Box sx={{ display: "flex" }}>
+          <SessionUpdate
+            id={session.id}
+            name={session.name}
+            start={session.start}
+            end={session.end}
+          />
+          <Tooltip title="세션 삭제하기" arrow>
+            <IconButton onClick={handleDelete}>
+              <DeleteForeverIcon />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Header>
+    </Wrapper>
   );
 }
