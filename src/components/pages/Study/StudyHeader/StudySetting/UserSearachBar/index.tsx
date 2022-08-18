528860f6b227d47cc02fc583b01a53f7857a8628
KingDonggyu <dgkim69166916@gmail.com>
2022-08-19 00:52:11 +0900

    feat: #56 유저 검색 자동완성 기능 구현

---
+++ src/components/pages/Study/StudyHeader/StudySetting/UserSearachBar/index.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent } from 'react';
+import IUserEntity from '../../../../../../core/entities/interfaces/iUser';
+import { TextField, Autocomplete, Box } from '@mui/material';
+
+interface UserSearchBarProps {
+  userName: string;
+  searched: IUserEntity[];
+  handleChange: (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+}
+
+export default function UserSearchBar({
+  userName,
+  searched,
+  handleChange,
+}: UserSearchBarProps) {
+  return (
+    <Autocomplete
+      autoHighlight
+      options={searched}
+      getOptionLabel={(option) => option.name}
+      renderOption={(props, option) => (
+        <Box
+          component='li'
+          sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
+          {...props}
+        >
+          <img
+            loading='lazy'
+            width='30'
+            src={option.imageUrl}
+            alt='Github Profile'
+            style={{ borderRadius: '100%' }}
+          />
+          {option.name}
+        </Box>
+      )}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label='사용자 이름'
+          variant='outlined'
+          size='small'
+          value={userName}
+          onChange={(e) => handleChange(e)}
+        />
+      )}
+    />
+  );
+}
--- src/components/pages/Study/StudyHeader/StudySetting/index.tsx
+++ src/components/pages/Study/StudyHeader/StudySetting/index.tsx
@@ -1,7 +1,10 @@
+import { useState, ChangeEvent } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import StudySetting from './StudySetting';
 import Study from '../../../../../di/Study';
+import debounce from '../../../../../utils/debounce';
+import IUserEntity from '../../../../../core/entities/interfaces/iUser';
 
 interface StudySettingProps {
   id: string;
@@ -13,9 +16,30 @@ export default function StudySettingContainer({ id }: StudySettingProps) {
     onSuccess: () => navigate('/'),
   });
 
+  const [userName, setUserName] = useState<string>('');
+  const [searched, setSearched] = useState<[IUserEntity] | []>([]);
+
+  const searchUser = (debounce((name: string) => {
+    Study.searchStudyMember(id, name).then((data) => {
+      setSearched(data)
+    })
+  }))
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setUserName(e.target.value);
+    searchUser(e.target.value);
+  }
+
   const handleDelete = () => {
     mutation.mutate(id);
   };
 
-  return <StudySetting handleDelete={handleDelete} />;
+  return (
+    <StudySetting
+      userName={userName}
+      searched={searched}
+      handleChange={handleChange}
+      handleDelete={handleDelete}
+    />
+  );
 }
