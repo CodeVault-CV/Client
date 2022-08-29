2e0e16925c8887fcfb3717b4c31291ad376e2898
woong-jae <jwchung0828@gmail.com>
2022-08-29 16:33:58 +0900

    feat: Solve, Solution 페이지 code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { lazy, Suspense } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./hoc/Layout";
 import Home from "./components/pages/Home";
 import Study from "./components/pages/Study";
-import Session from "./components/pages/Session";
-import Solve from "./components/pages/Solve";
 import NotFound from "./components/pages/NotFound";
-import Solution from "./components/pages/Solution";
+import Loading from "./components/blocks/Loading";
+
+const Session = lazy(() => import("./components/pages/Session"));
+const Solve = lazy(() => import("./components/pages/Solve"));
+const Solution = lazy(() => import("./components/pages/Solution"));
 
 const queryClient = new QueryClient();
 
@@ -15,16 +18,24 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="/study/:studyId" element={<Study />} />
-            <Route path="/study/:studyId/session/:sessionId" element={<Session />} />
-            <Route path="/study/:studyId/session/:sessionId/solve/:problemId" element={<Solve />} />
-            <Route path="/study/:studyId/session/:sessionId/solution/:solutionId" element={<Solution />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/study/:studyId" element={<Study />} />
+              <Route path="/study/:studyId/session/:sessionId" element={<Session />} />
+              <Route
+                path="/study/:studyId/session/:sessionId/solve/:problemId"
+                element={<Solve />}
+              />
+              <Route
+                path="/study/:studyId/session/:sessionId/solution/:solutionId"
+                element={<Solution />}
+              />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </QueryClientProvider>
   );
